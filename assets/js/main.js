e4cff0ce6aef99d304b1e2a23cecadbc743d8f10
Napomuceno <erjeffersonnapomuceno@gmail.com>
2023-10-02 09:20:24 -0300

    feat-Criando o card de detalhe

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,9 @@
 const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
+const PokemonModal = document.getElementById('PokemonModal');
+
+
+
 
 const maxRecords = 151
 const limit = 10
@@ -45,3 +49,83 @@ loadMoreButton.addEventListener('click', () => {
         loadPokemonItens(offset, limit)
     }
 })
+
+const toggleModal =() =>{
+    modal.classList.toggle("hide");
+    fade.classList.toggle("hide");
+}
+
+
+
+
+pokemonList.addEventListener('click', (event) => {
+    const clickedPokemon = event.target.closest('.pokemon');
+    if (clickedPokemon) {
+        const pokemonNumber = clickedPokemon.querySelector('.number').textContent.slice(1);
+        const pokemonName = clickedPokemon.querySelector('.name').textContent;
+        const pokemonType = clickedPokemon.querySelector('.type').textContent;
+        const pokemonImage = clickedPokemon.querySelector('img').getAttribute('src');
+
+        const pokemon = {
+            number: pokemonNumber,
+            name: pokemonName,
+            type: pokemonType,
+            photo: pokemonImage,
+        };
+
+        openModal(pokemon);
+    }   
+});
+
+
+function openModal(pokemon){
+    const typesHtml = pokemon.types ? `
+    <ol class="types">
+        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+    </ol>
+    ` : '';
+
+    const PokemonmodalContent = `
+      <div class="modalcabecalho ${pokemon.type}">
+        <button class="close-button" onclick="fecharModal()"></button>
+        <h1 Class="modalnome">${pokemon.name}</h1>
+        
+        <span class="modalNum">#${pokemon.number}</span>
+        <div>
+            <span class="pokemon ${pokemon.type}">typesHtml</span>
+        </div>
+
+        <img class="imagemmodal" src="${pokemon.photo}" alt="${pokemon.name}">
+       
+      </div>
+      <div class="modalcorpo">
+        <div class="corpoCabecalho">
+            <span>abount</span>
+        </div>
+
+        <div class="bodycorpo">
+          
+
+        </div>
+      </div>
+      
+      <div>rodape</div>
+
+    `;
+
+    PokemonModal.innerHTML = PokemonmodalContent;
+
+    PokemonModal.style.display = 'block';
+
+
+}
+
+function fecharModal() {
+    PokemonModal.style.display = 'none';
+}
+
+PokemonModal.addEventListener('click', (event) => {
+    if (event.target === PokemonModal) {
+        fecharModal();
+    }
+});
